adee126d94c5d91e40f7ce2debe6012ac0357c32
MadhuraneniSaiDinesh <msd010111@gmail.com>
2025-12-28 21:45:21 +0530

    first qr project

---
+++ QR_CODE.py
@@ -0,0 +1,31 @@
+# import qrcode from library
+
+import qrcode
+
+# Taking Upi id as Input
+upi_id = input('Enter your UPI id:- ')
+
+# upi://pay?pa=UPI_ID&apn=APP_NAME&am=Amount&cu=Currency&tn=MESSAGE
+
+# Defining the payment URL based on the UPI ID and the payment app
+# You can modify thes URLs based on the payment apps you want to support
+phone_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+google_pay_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+paytm_url = f'upi://pay?pa={upi_id}&pn=Recipient%20Name&mc=1234'
+
+
+# create QR Codes for each payment app
+phone_pay_qr = qrcode.make(phone_pay_url)
+google_pay_qr = qrcode.make(google_pay_url)
+paytm_qr = qrcode.make(paytm_url)
+
+
+# save the QR code to image file 
+phone_pay_qr.save('phone_pay_qr.png')
+google_pay_qr.save('google_pay_qr.png')
+paytm_qr.save('paytm_qr.png')
+
+#Display the QR codes (you may need to install PIL/Pillow Library)
+phone_pay_qr.show()
+google_pay_qr.show()
+paytm_qr.show()
